e15b10fae5bf86196393b8f4a3f0a8730353f78a
Carlos Garzón <113515873+Pacmaxito@users.noreply.github.com>
2022-10-10 12:08:05 +0200

    Ejercicio 3 Parte 9

---
+++ Precurso-Presencial-JS-master/parte09/03_ejercicio_crear_objeto/scriptO3.js
@@ -0,0 +1,11 @@
+let coche ={
+    color: "rojo" ,
+    marca: "Ford" ,
+    modelo: "focus" ,
+    motor: "cilindros 4, capacidad: 2.2" ,
+
+}
+
+console.log(coche);
+console.log(coche.modelo);
+console.log(coche.motor);
