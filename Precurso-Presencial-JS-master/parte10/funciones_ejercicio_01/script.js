5a66bfdd591c49f5d7a61bf84c116a35516e3763
Carlos Garzón <113515873+Pacmaxito@users.noreply.github.com>
2022-10-17 09:24:01 +0200

    Funciones Ej1

---
+++ Precurso-Presencial-JS-master/parte10/funciones_ejercicio_01/script.js
@@ -0,0 +1,20 @@
+
+
+
+function holafuntion(){
+
+    for (i=0; i < NumVueltas ; i++){
+
+    console.log("Hola");
+    };
+  
+}
+
+let respuesta = prompt("Dime un número");
+let NumVueltas = parseInt(respuesta);
+let i = 0;
+
+
+holafuntion();
+
+
