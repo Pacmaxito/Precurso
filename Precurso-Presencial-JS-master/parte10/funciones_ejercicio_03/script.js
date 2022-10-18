9aa7ae57f2dbfdb32e607075b0ac9c435cabd0d1
Carlos Garzón <113515873+Pacmaxito@users.noreply.github.com>
2022-10-18 10:52:36 +0200

    Funciones 3 parte 10 corregido

--- Precurso-Presencial-JS-master/parte10/funciones_ejercicio_03/script.js
+++ Precurso-Presencial-JS-master/parte10/funciones_ejercicio_03/script.js
@@ -8,14 +8,15 @@ function division(N1, N2){
 
 };
 
-   
-    
+let N1 = 0;
+let N2 = 0;
+
  do{
 
     let Respuesta1 = prompt("Introduce un número");
     let Respuesta2 = prompt("Introduce otro número");
-    let N1 = parseInt(Respuesta1);
-    let N2 = parseInt(Respuesta2);
+    N1 = parseInt(Respuesta1);
+    N2 = parseInt(Respuesta2);
     
     division(N1,N2);
 
