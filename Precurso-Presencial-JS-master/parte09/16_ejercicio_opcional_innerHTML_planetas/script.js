567c152fa09dd6ead959e344e3df5b41810793aa
Carlos Garzón <113515873+Pacmaxito@users.noreply.github.com>
2022-10-18 11:33:48 +0200

    Ejercicio 16 Planetas Corregido

--- Precurso-Presencial-JS-master/parte09/16_ejercicio_opcional_innerHTML_planetas/script.js
+++ Precurso-Presencial-JS-master/parte09/16_ejercicio_opcional_innerHTML_planetas/script.js
@@ -1,50 +1,68 @@
-
 let sistemaSolar = {
-    
-    Mercurio:{
-        Nombre: "Mercurio",
-        Caracteristicas: "Mercurio es el planeta más cercano al Sol, el más pequeño de los planetas internos. Se sabe de la existencia de Mercurio desde el siglo III a.d.C. En la mitología romana, Mercurio era el dios del comercio y los viajes, por eso se le dió este nombre al planeta, por su movimiento rápido a través del cielo.",
-        
+    mercurio: {
+    nombre: 'mercurio',
+    color: 'gris',
+    temperatura: 350,
+    imagen: 'https://www.astromia.com/fotosolar/fotos/planetamercurio.jpg'
     },
 
-    Venus:{
-        Nombre: "Venus",
-        Caracteristicas: "Venus es el segundo planeta desde el Sol. Se le considera la hermana del planeta Tierra por su similitud en tamaño y masa, y por ser el planeta más cercano. Sin embargo, su atmósfera está compuesta por gases tóxicos (90-95% dióxido de carbono) y temperaturas superiores a los 450ºC.",
+    venus: {
+    nombre: 'venus',
+    color: 'blanco',
+    temperatura: 460,
+    imagen :
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Venus-real_color.jpg/220px-Venus-real_color.jpg'
     },
 
-    Tierra:{
-        Nombre: "Tierra",
-        Caracteristicas: "La Tierra es el tercer planeta desde el Sol y el mayor de los planetas internos. Es el quinto más grande en tamaño en el sistema solar. Como Mercurio y Venus, la Tierra se formó a partir de una masa caliente y fundida que luego se enfrió y solidificó.",
+    tierra: {
+    nombre: 'tierra',
+    color: 'morado',
+    temperatura: 14,
+    imagen :
+    'https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/5a0ab2a75bafe87f4d3f9a63/acaba-tiempo_0.jpg'
     },
-
-    Marte:{
-        Nombre: "Marte",
-        Caracteristicas: "Marte es el cuarto planeta desde el Sol y el último de los planetas internos. Desde la Tierra se puede observar a Marte que luce rojo por la gran cantidad de polvo que cubre la superficie, por lo que se le conoce como el planeta rojo.",
+    marte: {
+    nombre: 'marte',
+    color: 'rojo',
+    temperatura: -46,
+    imagen :
+    'https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/55365b6b34099b0279c8fb2e/slide-marte.jpg'
     },
-
-    Jupiter:{
-        Nombre: "Jupiter",
-        Caracteristicas: "Júpiter es el primero de los planetas gaseosos y el quinto desde el Sol. Es el más grande de los planetas del sistema solar, concentrando el 71% de todo el material del sistema, exceptuando al Sol.",
+    jupiter: {
+    nombre: 'jupiter',
+    color: 'marrón',
+    temperatura: -121,
+    imagen :
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Jupiter_by_Cassini-Huygens.jpg/280px-Jupiter_by_Cassini-Huygens.jpg'
     },
-
-    Saturno:{
-        Nombre: "Saturno",
-        Caracteristicas: "Saturno es el sexto planeta desde el Sol y el segundo más grande del sistema solar. Su característica más distintiva es el sistema de anillos que lo circunda. En total son siete anillos: A, B, C, D, E, F y G.",
+    saturno: {
+    nombre: 'saturno',
+    color: 'amarillo',
+    temperatura: -130,
+    imagen :
+    'https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/558d2b15010e26221b57df75/saturno.jpg'
     },
-        
-    Urano:{
-        Nombre: "Urano",
-        Caracteristicas: "Urano es el tercero de los planetas gaseosos y el séptimo desde el Sol. Fue descubierto por el astrónomo William Herschel en 1781. Es un planeta helado, azul pálido y es el más lejano de los planetas que puede ser visto a simple vista desde la Tierra.",
+    urano: {
+    nombre: 'urano',
+    color: 'azul',
+    temperatura: -205,
+    imagen : 'http://www.manzanares.es/v2/sites/paseo/img/urano/urano1.jpg'
     },
-
-     Neptuno:{
-        Nombre: "Neptuno",
-        Caracteristicas: "El octavo y último planeta del sistema solar es Neptuno. Es el más pequeño de los planetas gaseosos, de color azul intenso y nubes blancas. Como los otros planetas gaseosos, posee un sistema de anillos alrededor del planeta.",
+    neptuno: {
+    nombre: 'neptuno',
+    color: 'azul',
+    temperatura: -220,
+    imagen :
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Neptune_Full.jpg/280px-Neptune_Full.jpg'
     },
-    
-
-   
-}
+    pluton: {
+    nombre: 'plutón',
+    color: 'blanco',
+    temperatura: -229,
+    imagen :
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Nh-pluto-in-true-color_2x_JPEG.jpg/1200px-Nh-pluto-in-true-color_2x_JPEG.jpg'
+    }
+    };
 
 
 let PLanetasElegidos ={
@@ -68,8 +86,8 @@ if (sistemaSolar[Planeta1].Nombre == PLanetasElegidos.Planeta1);{
     carta.innerHTML = `
     <div class="planeta1"> 
     <h2 class="NombreP1"> ${Planeta1} </h2>
-    <img class="ImagenP1">
-    <p class="CaracterísticasP1">${sistemaSolar[Planeta1].Caracteristicas}</p>
+    <img src="${sistemaSolar[Planeta1].imagen}">
+    <p class="CaracterísticasP1"> Temperatura <br>  ${sistemaSolar[Planeta1].temperatura}</p>
     </div>`;
     
     
@@ -82,8 +100,8 @@ if (sistemaSolar[Planeta2].Nombre == PLanetasElegidos.Planeta2);{
     carta.innerHTML = `
     <div class="planeta2">
             <h2 class="NombreP2"> ${Planeta2} </h2>
-            <img class="ImagenP2">
-            <p class="CaracterísticasP2">${sistemaSolar[Planeta2].Caracteristicas}</p>
+            <img src="${sistemaSolar[Planeta2].imagen}">
+            <p class="CaracterísticasP2"> Temperatura <br> ${sistemaSolar[Planeta2].temperatura}</p>
         </div>`;
     }
 
@@ -94,8 +112,8 @@ if (sistemaSolar[Planeta3].Nombre == PLanetasElegidos.Planeta3);{
     carta.innerHTML = `
     <div class="planeta3">
         <h2 class="NombreP2"> ${Planeta3} </h2>
-        <img class="ImagenP3">
-        <p class="CaracterísticasP3">${sistemaSolar[Planeta3].Caracteristicas}</p>
+        <img src="${sistemaSolar[Planeta3].imagen}">
+        <p class="CaracterísticasP3"> Temperatura <br> ${sistemaSolar[Planeta3].temperatura}</p>
 
         </div>`;
     }
